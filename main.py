09dd41b0c85aaffc9c3e3d36ab332a157e79d95c
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2023-06-08 23:20:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,24 @@
 É um programa facilitador com várias funções para o dia a dia de uma pequena empresa,
 com vários facilitadores e recurso de edição de documentos a funções na empresa
 """
+import tkinter
+import customtkinter
+
+
+root = customtkinter.CTk()
+
+class Application():
+    def __init__(self):
+        self.root = root
+        self.telaPrincipal()
+        root.mainloop()
+    def telaPrincipal():
+        self.root.title("Seshat - Administração Empresarial Profissionalizada")
+        self.root.configure(background='gray')
+        self.root.geometry('640x480')
+        self.root.resizable(True, True)
+        self.root.maxsize(width= 1600, height= 1200)
+    
+
+
+Application()
