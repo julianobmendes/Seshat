e61565367808fe7407eb1c13f253dee8ce4e8812
Juliano Boaventura Mendes <julianomendes_@hotmail.com>
2023-06-09 16:56:50 -0300

    red

--- main.py
+++ main.py
@@ -6,21 +6,29 @@ com vários facilitadores e recurso de edição de documentos a funções na emp
 import tkinter
 import customtkinter
 
+customtkinter.set_appearance_mode('dark')
+customtkinter.set_default_color_theme('dark-blue')
 
 root = customtkinter.CTk()
 
-class Application():
+
+class Application:
     def __init__(self):
         self.root = root
         self.telaPrincipal()
+        self.frames_de_tela()
         root.mainloop()
-    def telaPrincipal():
+
+    def telaPrincipal(self):
         self.root.title("Seshat - Administração Empresarial Profissionalizada")
-        self.root.configure(background='gray')
         self.root.geometry('640x480')
         self.root.resizable(True, True)
-        self.root.maxsize(width= 1600, height= 1200)
-    
+        self.root.maxsize(width=1600, height=1200)
+        self.root.minsize(width=640, height=480)
+    def frames_de_tela(self):
+        self.frame_01 = customtkinter.CTkFrame(self.root)
+        self.frame_01.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.96)
+
 
 
 Application()
